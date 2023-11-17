ec56ba0f2c2f450774a50a5fee721e0302c524d2
Ondřej Bárta <email@ondrejbarta.xyz>
2023-11-17 14:34:10 +0100

    Initial Commit

---
+++ Glassnode At A Glance/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Glassnode At A Glance
+//
+//  Created by Ondrej Barta on 17.11.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Glassnode At A Glance/Glassnode_At_A_GlanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Glassnode_At_A_GlanceApp.swift
+//  Glassnode At A Glance
+//
+//  Created by Ondrej Barta on 17.11.23.
+//
+
+import SwiftUI
+
+@main
+struct Glassnode_At_A_GlanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
